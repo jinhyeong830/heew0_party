b2fd15fb77b1300da77e3c0987e48f58ba0e1394
allie <allie@spreatics.com>
2024-02-29 17:52:10 +0900

    cross env 설정 및 배포/개발 config 분리

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 'use strict';
 const Sequelize = require('sequelize');
-
-const config = require(__dirname + '/../config/config.js')['prod'];
+// console.log(process.env.DB_MODE);
+console.log(process.env.NODE_ENV);
+const config = require(__dirname + '/../config/config.js')[process.env.NODE_ENV];
 console.log(config);
 const db = {};
 
