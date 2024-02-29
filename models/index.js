8e77220c7d824a4f58ac9d8480d80ee3900e0103
allie <allie@spreatics.com>
2024-02-29 18:02:39 +0900

    fff

--- models/index.js
+++ models/index.js
@@ -2,7 +2,13 @@
 const Sequelize = require('sequelize');
 // console.log(process.env.DB_MODE);
 console.log(process.env.NODE_ENV);
-const config = require(__dirname + '/../config/config.js')[process.env.NODE_ENV];
+
+// 수정할 수 있는 부분 수정하기
+if (process.env.NODE_ENV) {
+  const config = require(__dirname + '/../config/config.js')[process.env.NODE_ENV];
+} else {
+  const config = require(__dirname + '/../config/config.js')['development'];
+}
 console.log(config);
 const db = {};
 
